c9ed6173bc3c3347f73b82ec69127723ed7b912a
Nabil Merchant <nabil@Nabils-MacBook-Pro.local>
2019-05-02 22:39:23 -0400

    Something broke and I am trying to figure out what...

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,16 +5,27 @@ session = boto3.Session()
 ec2 = session.resource('ec2')
 
 @click.command()
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
 def list_instances():
     "List EC2 instances."
-    for i in ec2.instances.all():
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = { t['Key']:t['Value'] for t in i.tags or [] }
         print(', '.join((
-        i.id,
-        i.instance_type,
-        i.placement['AvailabilityZone'],
-        i.state['Name'],
-        i.public_dns_name
-        )))
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('Project', '<no project>'))))
 
     return
 
